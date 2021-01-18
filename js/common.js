4dd42bad70930202bdcf960234529cf869624763
jhw811 <jhw811@gmail.com>
2021-01-18 20:58:48 +0900

    no message

--- js/common.js
+++ js/common.js
@@ -11,3 +11,14 @@ $(document).ready(function () {
   isMob = width < 640;
   resize();
 });
+
+function findGetParameter(parameterName) {
+  var result = null,
+    tmp = [];
+  var items = location.search.substr(1).split('&');
+  for (var index = 0; index < items.length; index++) {
+    tmp = items[index].split('=');
+    if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+  }
+  return result;
+}
